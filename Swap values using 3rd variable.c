b99b0b30b766f5859c82bba52703f6e824a6613c
prfnlcoder00 <118184901+prfnlcoder00@users.noreply.github.com>
2022-11-14 05:16:10 -0800

    Add files via upload

---
+++ Swap values using 3rd variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a;
+	int b = 20;
+	int c;
+    scanf("%d",&a);		
+	
+	
+	printf("Swapped Value for a :- %d\n",a);
+	printf("Swapped Value for b :- %d",b);
+	
+
+	
+	
+	
+	
+	
+}
---
+++ Swapped variable without using 3rd variable.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a = 10;
+	int b = 20;
+	
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	
+	printf("Swapped value for a :- %d\n",a);
+	printf("Swapped value for b :- %d",b); 
+	
+	
+	
+	
+	
+	
+}
